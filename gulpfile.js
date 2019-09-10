a89933c753a3c92794841fb3e2d2510cca9d916d
lich <100108350@qq.com>
2019-09-11 01:57:39 +0800

    添加angularjs的html gulp打包

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ const autoprefixer = require('autoprefixer');
 const postcss = require('gulp-postcss');
 const browserSync = require('browser-sync').create();
 
+// 添加angular-html打包处理
+const templateCache = require('gulp-angular-templatecache');
 
 const wireDep = require('wiredep').stream;
 
@@ -39,14 +41,32 @@ gulp.task('complie.scss',()=>
         .pipe(browserSync.reload({stream:true}))    
 );
 
+gulp.task('complie.scss',()=>
+    gulp.src('src/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error',sass.logError))
+        .pipe(postcss([autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')]))
+        .pipe(sourcemaps.write(''))
+        .pipe(gulp.dest(gulpconfig.baseDir + '/css'))
+        .pipe(browserSync.reload({stream:true}))    
+);
+
 gulp.task('index.html',()=>
     gulp.src('src/index.html')
         .pipe(wireDep())
         .pipe(gulp.dest(gulpconfig.baseDir))
+        .pipe(browserSync.reload({stream:true})) 
 );
 
+gulp.task('templates', function () {
+    return gulp.src('src/app/**/*.html')
+        .pipe(templateCache({module:'BlankApp'}))
+        .pipe(gulp.dest(gulpconfig.baseDir))
+        .pipe(browserSync.reload({stream:true})); 
+});
+
 // 静态服务器
-gulp.task('serve',gulp.series(['complie.js','complie.scss','index.html', function() {
+gulp.task('serve',gulp.series(['complie.js','complie.scss','index.html','templates', function() {
     browserSync.init({
         server: {
             baseDir: gulpconfig.baseDir
@@ -54,6 +74,8 @@ gulp.task('serve',gulp.series(['complie.js','complie.scss','index.html', functio
     });
     gulp.watch("src/**/*.js", gulp.series(['complie.js']));
     gulp.watch("src/**/*.scss", gulp.series(['complie.scss']));
+    gulp.watch("src/index.html", gulp.series(['index.html']));
+    gulp.watch("src/app/**/*.html", gulp.series(['templates']));
 }]));
 
 
--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 console.log(angular);
+angular.module('BlankApp',[],function(){
+    console.log(1);
+}).run(()=>{
+    new Promise((resolve,reject)=>{
+        console.log(2);
+        resolve(2);
+    });
+  
+});
